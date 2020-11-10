a200dcd7ca668ccb375b5b12fe0478ee8b2130bb
Caleb-Mwasikira <calebmwasikira@gmail.com>
2020-11-10 07:47:42 +0300

    :lock: Encrypting all the data being sent/received between the client and server

---
+++ encryption/rsa_encryption.py
@@ -0,0 +1,143 @@
+import os
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature import pkcs1_15
+from Crypto.Cipher import PKCS1_OAEP
+
+from urls.__init__ import ROOT
+
+
+class RSAEncryption:
+    def __init__(self):
+        self.private_key = None
+        self.public_key = None
+
+    @staticmethod
+    def genKeys():
+        print(f"[+] Generating new pair of private and public keys")
+        BITS = 2048
+        private_key = RSA.generate(BITS)
+        public_key = private_key.publickey()
+        return private_key, public_key
+
+    @staticmethod
+    def pemSaveKey(key, key_file_path, pass_phrase):
+        """Saving an encrypted key onto a .pem file"""
+        try:
+            print(f"[*] Saving encryption key to file")
+            keys_folder = os.path.dirname(key_file_path)
+
+            if not os.path.exists(keys_folder):
+                os.makedirs(keys_folder)
+
+            with open(key_file_path, "wb+") as f:
+                f.write(key.export_key(format="PEM", passphrase=pass_phrase, pkcs=8))
+            print(f"[+] Key successfully saved on file {key_file_path}")
+            return True
+
+        except FileNotFoundError as err:
+            print(f"[-] Error: {err}")
+
+    def pemLoadKey(self, key_file_path, pass_phrase):
+        """Importing an encrypted key from a file"""
+        try:
+            print(f"[*] Loading encryption key from file ...")
+            with open(key_file_path, "rb") as f:
+                data = f.read()
+                key = RSA.import_key(data, passphrase=pass_phrase)
+
+            if key.has_private():  # if key is a private key, generate its corresponding public key
+                self.private_key = key
+                self.public_key = self.private_key.publickey()
+
+            return key
+
+        except ValueError as error:
+            print(f"[-] Error: {error}")
+            return False
+
+        except FileNotFoundError as error:
+            print(f"[!] Warning : {error}")
+            return False
+
+    @staticmethod
+    def signMsg(msg, private_key):
+        """Signs a message with a private key"""
+        signer = pkcs1_15.new(private_key)  # Instantiate a new signer object using the senders private key
+        hashed_msg = SHA256.new(msg)
+        signature = signer.sign(hashed_msg)
+        print(f"[+] Message signed successfully")
+        return signature
+
+    @staticmethod
+    def verifySignedMsg(msg, signature, public_key):
+        """Verifies a signature to see if the message is authentic"""
+        try:
+            print(f"[*] Verifying message authenticity")
+            verifier = pkcs1_15.new(public_key)  # Instantiate a new verifier object using the senders public key
+            hashed_msg = SHA256.new(msg)
+            verifier.verify(hashed_msg, signature)
+            print(f"[+] Message authenticated successfully")
+            return True
+
+        except ValueError as error:
+            print(f"[-] Error : {error}")
+            return False
+
+    @staticmethod
+    def encryptMsg(plain_text, public_key):
+        try:
+            # print(f"[*] Encrypting message")
+            if type(plain_text) != bytes:
+                plain_text = plain_text.encode("utf-8")
+
+            cipher = PKCS1_OAEP.new(public_key)
+            cipher_text = cipher.encrypt(plain_text)
+            # print(f"[+] Message encrypted successfully")
+            return cipher_text
+
+        except AttributeError as err:
+            print(f"[-] Error: {err}")
+            return False
+
+    @staticmethod
+    def decryptMsg(cipher_text, private_key):
+        try:
+            # print(f"[*] Decrypting message")
+            cipher = PKCS1_OAEP.new(private_key)
+            plain_text = cipher.decrypt(cipher_text)
+            return plain_text
+
+        except ValueError as error:
+            raise Exception(f'Message decryption failed: {error}')
+
+
+def Main():
+    message = b"My deep dark secrets"
+
+    PRIVATE_KEY_FILE = f"{ROOT}/keys/private_key.pem"
+    PUBLIC_KEY_FILE = f"{ROOT}/keys/public_key.pem"
+    PASSWORD = "Sixteen byte keys"
+
+    rsa_encryptor = RSAEncryption()
+    private_key = rsa_encryptor.pemLoadKey(PRIVATE_KEY_FILE, PASSWORD)
+
+    if private_key:
+        public_key = rsa_encryptor.public_key
+    else:
+        private_key, public_key = rsa_encryptor.genKeys()
+        rsa_encryptor.pemSaveKey(private_key, PRIVATE_KEY_FILE, PASSWORD)
+        rsa_encryptor.pemSaveKey(public_key, PUBLIC_KEY_FILE, PASSWORD)
+
+    cipher_text = rsa_encryptor.encryptMsg(message, public_key)
+    print(cipher_text)
+
+    plain_text = rsa_encryptor.decryptMsg(cipher_text, private_key)
+    print(plain_text)
+
+    sig = rsa_encryptor.signMsg(message, private_key)
+    rsa_encryptor.verifySignedMsg(message, sig, public_key)
+
+
+if __name__ == "__main__":
+    Main()
--- lib/endpoint.py
+++ lib/endpoint.py
@@ -1,8 +1,13 @@
 import toml
 import logging
+import os
 import pickle
 import colorama as colors
+from getpass import getpass
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
+from encryption.rsa_encryption import RSAEncryption
 from urls.__init__ import ROOT
 
 
@@ -19,7 +24,7 @@ class EndPoint:
         conf_filename = fr"{ROOT}/settings/config.toml"
         config = self.readConfigFile(conf_filename)
         self.app_conf, project_conf = config['APP'], config['PROJECT']
-        # keys_conf, redis_db_conf = config['KEYS'], config['REDIS-DATABASE']
+        keys_conf, redis_db_conf = config['KEYS'], config['REDIS-DATABASE']
 
         # Ready log books
         log_filename = fr"{ROOT}/logs/log_book.log"
@@ -28,6 +33,49 @@ class EndPoint:
         self.HOST = project_conf['HOST']
         self.PORT = project_conf['PORT']
 
+        # Initialise RSA encryption
+        self.rsa_encryptor, self.private_key, self.public_key = self.initRSAEncryption(keys_conf)
+
+        # AES encryption
+        self.AES_KEY = None
+        self.AES_IV = None
+
+    @staticmethod
+    def initRSAEncryption(keys_conf):
+        print(f"[*] Setting up a secure connection")
+        private_key_file: str = os.path.join(ROOT, keys_conf['PRIVATE_KEY_FILE'])
+        public_key_file: str = os.path.join(ROOT, keys_conf['PUBLIC_KEY_FILE'])
+        password: str = keys_conf['PASSWORD']
+
+        while password is None:
+            password = getpass(prompt="Please enter your password to continue >> ")
+            if len(password) >= 6:
+                break
+            else:
+                print(f"[!] Password not secure")
+
+        rsa_encryptor = RSAEncryption()
+        private_key = rsa_encryptor.pemLoadKey(private_key_file, password)
+
+        if private_key:
+            public_key = rsa_encryptor.public_key
+
+        else:
+            private_key, public_key = rsa_encryptor.genKeys()
+            rsa_encryptor.pemSaveKey(private_key, private_key_file, password)
+            rsa_encryptor.pemSaveKey(public_key, public_key_file, password)
+
+        return rsa_encryptor, private_key, public_key
+
+    @staticmethod
+    def initAESEncryption(key: bytes, iv: bytes = None):
+        key_size = 16
+        iv_size = 16
+        AES_KEY = key[:key_size] if len(key) > key_size else pad(key, key_size)
+        AES_IV = iv or os.urandom(iv_size)
+        assert(len(AES_KEY) == key_size)  # key must a be 8, 16, 32 byte key
+        assert(len(AES_IV) == iv_size)  # initialization vector must be 16 bytes long
+        return AES_KEY, AES_IV
 
     @staticmethod
     def readConfigFile(conf_filename):
@@ -48,26 +96,42 @@ class EndPoint:
         print(f"[*] Sending msg to {self.HOST}:{self.PORT}")
         data: bytes = pickle.dumps(msg)
 
-        if data:
-            _socket.send(data)
+        if self.AES_KEY and self.AES_IV:
+            cipher = AES.new(self.AES_KEY, AES.MODE_CBC, self.AES_IV)
+            cipher_text = cipher.encrypt(pad(data, AES.block_size))
+        else:
+            cipher_text = self.rsa_encryptor.encryptMsg(data, self.public_key)
+
+        if cipher_text:
+            _socket.send(cipher_text)
             print(f"[+] Msg sent successfully")
             return True
         else:
             raise Exception('Message encryption failed')
 
+
     def receiveMsg(self, _socket):
         BYTES = 1024
-        data: bytes = _socket.recv(BYTES)
+        encrypted_data: bytes = _socket.recv(BYTES)
 
-        print(f"\n[+] Msg received from {self.HOST}:{self.PORT}")
+        if encrypted_data:
+            print(f"\n[+] Msg received from {self.HOST}:{self.PORT}")
 
+            if self.AES_KEY and self.AES_IV:
+                cipher = AES.new(self.AES_KEY, AES.MODE_CBC, self.AES_IV)
+                plain_text = unpad(cipher.decrypt(encrypted_data), AES.block_size)
+            else:
+                plain_text = self.rsa_encryptor.decryptMsg(encrypted_data, self.private_key)
 
-        if data:
-            data: dict = pickle.loads(data)
-            return data
+            if plain_text:
+                data: dict = pickle.loads(plain_text)
+                return data
 
+            else:
+                raise Exception('Failed to decrypt encrypted message')
         else:
-            raise Exception('No message received')
+            raise ConnectionResetError("No data received from endpoint. Current connection might be down")
+
 
     def closeConnection(self, socket, HOST, PORT):
         message = f"Closing connection with {HOST}:{PORT}. Connection aborted."
